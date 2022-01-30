326837344d8e4b4a1acfbdfdb1670a301eae7204
Mohamed Gouda <muhammed.gouda93@gmail.com>
2022-01-30 10:00:30 +0200

    adding more tests

--- test/test.js
+++ test/test.js
@@ -20,12 +20,30 @@ test('should signup new user' , async ()=>{
 })
 
 
-test('should signin new user' , async ()=>{
+test('should not signup new user' , async ()=>{
+    await request(app).post('/users').send({
+            "name":"bosta",
+            "age":"5",
+            "password":"bosta1234",
+            "email":"bosta@bosta.com"
+        }).expect(400)
+})
+
+
+test('should signin  user' , async ()=>{
     await request(app).post('/login').send({
             "password":"bosta1234",
             "email":"bosta@bosta.com"
         }).expect(200)
 })
+
+test('should not signin  user' , async ()=>{
+    await request(app).post('/login').send({
+            "password":"bosta14",
+            "email":"bosta@bosta.com"
+        }).expect(200)
+})
+
 
 test('should create new check' , async ()=>{
     await request(app).post('/checks')
@@ -36,6 +54,26 @@ test('should create new check' , async ()=>{
         "protocol":"https"
     }).expect(200)
 })
+
+test('should not create new check' , async ()=>{
+    await request(app).post('/checks')
+        .set('Authorization',`Bearer ${token}`)
+        .send({
+        "url":"https://www.google.fr/?gws_rd=ssl",
+        "protocol":"https"
+    }).expect(400)
+})
+
+test('should create new check' , async ()=>{
+    await request(app).post('/checks')
+        .set('Authorization',`Brer ${token}`)
+        .send({
+        "name":"checkGoogle",
+        "url":"https://www.google.fr/?gws_rd=ssl",
+        "protocol":"https"
+    }).expect(401)
+})
+
 
 
 test('should create new check' , async ()=>{
@@ -64,6 +102,7 @@ test('should run  check' , async ()=>{
         .send().expect(200)
 })
 
+
 test('should run  check' , async ()=>{
     await request(app).post('/checks/Run/checkGithub')
         .set('Authorization',`Bearer ${token}`)
@@ -73,6 +112,18 @@ test('should run  check' , async ()=>{
 test('should run  check' , async ()=>{
     await request(app).post('/checks/Run/checklinkedin')
         .set('Authorization',`Bearer ${token}`)
+        .send().expect(200)
+})
+
+test('should not run  check' , async ()=>{
+    await request(app).post('/checks/Run/chkGoogle')
+        .set('Authorization',`Bearer ${token}`)
+        .send().expect(404)
+})
+
+test('should not run  check' , async ()=>{
+    await request(app).post('/checks/Run/checkGoogle')
+        .set('Authorization',`Beer ${token}`)
         .send().expect(200)
 })
 
@@ -92,8 +143,56 @@ test('should delete  check' , async ()=>{
         .send().expect(200)
 })
 
+
+
 test('should report check' , async ()=>{
     await request(app).get('/checks/checkGoogleNewName/report')
         .set('Authorization',`Bearer ${token}`)
         .send().expect(200)
+})
+
+
+test('should not delete  check' , async ()=>{
+    await request(app).patch('/checks/checkGoogewName')
+        .set('Authorization',`Bearer ${token}`)
+        .send().expect(404)
+})
+
+test('should not delete  check' , async ()=>{
+    await request(app).patch('/checks/checkGoogewName')
+        .set('Authorization',`Berer ${token}`)
+        .send().expect(401)
+})
+
+test('should not report check' , async ()=>{
+    await request(app).get('/checks/checkGoogleNewNa/report')
+        .set('Authorization',`Bearer ${token}`)
+        .send().expect(404)
+})
+
+test('should not report check' , async ()=>{
+    await request(app).get('/checks/checkGoogleNewNa/report')
+        .set('Authorization',`Barer ${token}`)
+        .send().expect(401)
+})
+
+
+test('should not edit  check' , async ()=>{
+    await request(app).patch('/checks/checkGoogle')
+        .set('Authorization',`Bearer ${token}`)
+        .send({
+            "name":"checkGoogewName",
+            "url":"https://www.google.fr/?gws_rd=ssl",
+            "protocol":"https"
+        }).expect(404)
+})
+
+test('should not edit  check' , async ()=>{
+    await request(app).patch('/checks/checkGoogle')
+        .set('Authorization',`Beaer ${token}`)
+        .send({
+            "name":"checkGoogleNewName",
+            "url":"https://www.google.fr/?gws_rd=ssl",
+            "protocol":"https"
+        }).expect(401)
 })
